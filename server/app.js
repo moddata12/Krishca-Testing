192b0d18e4c56b03f3d18c726449fd801837e3f5
moddata12 <143089733+moddata12@users.noreply.github.com>
2024-10-01 17:47:05 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://13.51.201.210:3001',
+    origin: 'http://127.0.0.1:3001',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
