258e29ae1f61a07b64ef2b0ab577e4e9502bfda3
moddata12 <143089733+moddata12@users.noreply.github.com>
2024-10-01 18:13:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config({path:path.join(__dirname, "config/config.env")});
 
 connectDatabase();
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server listening the port ${process.env.PORT} in ${process.env.NODE_ENV}`)
 })
 
